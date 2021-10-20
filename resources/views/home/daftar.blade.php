c5b4f5e7792337ac0b6ac8eabbc65c61ad3bbc85
kelasweekend <kelasweekend.id@gmail.com>
2021-10-20 14:40:15 +0700

    versi 4

--- resources/views/home/daftar.blade.php
+++ resources/views/home/daftar.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -203,4 +203,145 @@
     </script>
 </body>
 
-</html>
+</html> --}}
+
+@extends('layouts.home')
+@section('title')
+    Daftar Team Baru
+@endsection
+
+@section('content')
+    <!-- Header -->
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img src="{{ asset('frontend/img/pendaftaran.png') }}" class="img-fluid w-75 mx-auto d-block mt-5"
+                        alt="Header Img" />
+                </div>
+                <div class="col-sm-12 col-md-6 order-md-last">
+                    <h4 class="font-weight-bolder">
+                        Mulai Daftarkan Team Terbaik Anda
+                    </h4>
+                    <a href="#daftar" class="btn primary-color btn-rounded"><i class="fas fa-plus mr-1"></i> Daftar</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- End of Header -->
+    <section class="mt-5 mb-5" id="daftar">
+        <div class="container">
+            <div class="card rounded p-4 shadow-sm">
+                <form class="form-horizontal" method="POST" action="{{ route('daftar') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="email" class="form-label">Email Aktif</label>
+                                <input type="email" name="email" class="form-control" id="nama_ketua"
+                                    placeholder="E.g Setiawan@gmail.com" autocomplete="off" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <div class="form-group">
+                                    <label>kategori</label>
+                                    <select class="form-control" name="kategori_id">
+                                        <option aria-readonly="true">Pilih kategori</option>
+                                        @foreach ($kategori as $item)
+                                            <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="nama_team" class="form-label">Nama Team </label>
+                                <input type="text" name="nama_team" class="form-control" id="nama_team"
+                                    placeholder="E.g Aligator" autocomplete="off" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="nama_ketua" class="form-label">Nama Ketua Team</label>
+                                <input type="text" name="nama_ketua" class="form-control" id="nama_ketua"
+                                    placeholder="E.g Setiawan" autocomplete="off" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="instansi" class="form-label">Asal Instansi</label>
+                                <input type="text" name="instansi" class="form-control" id="instansi"
+                                    placeholder="E.g Aligator" autocomplete="off" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <div class="form-group">
+                                    <label>Tingkatan</label>
+                                    <select class="form-control" name="tingkatan" onchange="getval(this);">
+                                        <option aria-readonly="true">Pilih Tingkatan</option>
+                                        <option value="sma/smk">SMA / SMK Sederajat</option>
+                                        <option value="kuliah">Universitas</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row d-none" id="pendamping">
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="nama_pendamping" class="form-label">Nama Pendamping Team</label>
+                                <input type="text" name="nama_pendamping" class="form-control" id="nama_pendamping"
+                                    placeholder="E.g Budi skuy" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-2">
+                                <div class="form-group">
+                                    <label class="form-label">Berkas Pendamping Team</label>
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="berkas_pendamping"
+                                            id="customFile">
+                                        <label class="custom-file-label" for="customFile">KTP Pendamping</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <div class="anggota_1">
+                            <p class="text-muted mb-2">Anggota 1</p>
+                            <div class="d-flex justify-content-between">
+                                <input type="text" name="anggota_1" class="form-control mr-2" id="anggota_1"
+                                    placeholder="E.g Budi skuy" autocomplete="off" required>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="berkas_anggota_1" id="customFile"
+                                        required>
+                                    <label class="custom-file-label" for="customFile">upload berkas</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="anggota_2 mt-2">
+                            <p class="text-muted mb-2">Anggota 2</p>
+                            <div class="d-flex justify-content-between">
+                                <input type="text" name="anggota_2" class="form-control mr-2" id="anggota_2"
+                                    placeholder="E.g Budi skuy" autocomplete="off">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="berkas_anggota_2" id="customFile">
+                                    <label class="custom-file-label" for="customFile">upload berkas</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-secondary tombol float-right mt-3" type="submit">Submmit</button>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
